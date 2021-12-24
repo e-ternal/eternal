21d4866780a8dc872e553b561418ecb0d816b6fd
jackson <48326524+jpasjax@users.noreply.github.com>
2021-12-24 15:41:42 -0500

    1

--- src/commands/information/banner.js
+++ src/commands/information/banner.js
@@ -1,56 +1,52 @@
-const fetch = require('node-fetch')
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require("discord.js")
+const axios = require("axios")
 
 module.exports = {
-  name: "banner",
-  aliases: ['ub', "userbanner"],
+    name: "banner",
+    description: "Shows the enlarged version of your's or the mentioned user's banner.",
+    usage: "<member>",
+    run: async(client, message, args) => {
+        
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
 
-  run: async (client, message, args) => {
-    let mentionedMember = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || args[0] || message.member;
+        if(!user) return message.reply("An unknown error has occured.")
 
-    const user = await client.users.fetch(client.users.resolveID(mentionedMember)).catch(() => null);
-    if (!user) user = message.author;
-    let uid = user.id
-    let response = fetch(`https://discord.com/api/v8/users/${uid}`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bot ${client.token}`
-      }
-    })
-    let receive = ''
-    let banner = 'https://cdn.discordapp.com/attachments/829722741288337428/834016013678673950/banner_invisible.gif'
-    response.then(a => {
-      if (a.status !== 404) {
-        a.json().then(data => {
-          receive = data['banner']
-          if (receive !== null) {
-            let response2 = fetch(`https://cdn.discordapp.com/banners/${uid}/${receive}.gif`, {
-              method: 'GET',
-              headers: {
+        axios.get(`https://discord.com/api/users/${user.id}`, {
+            
+            headers: {
                 Authorization: `Bot ${client.token}`
-              }
-            })
-            let statut = ''
-            response2.then(b => {
-              statut = b.status
-              banner = `https://cdn.discordapp.com/banners/${uid}/${receive}.gif?size=1024`
-              if (statut === 415) {
-                banner = `https://cdn.discordapp.com/banners/${uid}/${receive}.png?size=1024`
-              }
-            })
-          }
+            },
         })
-      }
-    })
-    setTimeout(() => {
-      let embed = new MessageEmbed()
-        .setColor(mentionedMember.displayHexColor || color)
-        .setImage(banner)
-        .setURL(banner)
-        .setAuthor(message.author.username, message.author.avatarURL())
-        .setTitle(user.username + "'s banner")
-      message.channel.stopTyping(true);
-      message.channel.send({ embeds: [embed] });
-    }, 1000)
-  }
+
+        .then((res) => {
+            const { banner, accent_color } = res.data
+
+            if(banner)  {
+
+                
+                const extension = banner.startsWith("a_") ? '.gif' : '.png'
+
+                
+                const image = `https://cdn.discordapp.com/banners/${user.id}/${banner}${extension}?size=2048`;
+               
+                
+
+                const embed = new MessageEmbed()
+                .setTitle(`${user.user.tag}'s Banner`)
+                .setImage(image)
+                .setColor("RANDOM")
+                message.reply({embeds: [embed]})
+            } else {
+                if(accent_color) {
+                const embed = new MessageEmbed()
+                .setDescription(`${user.user.tag} does not have a banner, but they do have an accent color.`)
+                .setColor(accent_color)
+
+                message.reply({embeds: [embed]})
+                } else {
+                    message.reply(`${user.user.tag} does not have a banner nor an accent color.`)
+                }
+            }
+        })
+    }
 }
